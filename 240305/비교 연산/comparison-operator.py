672fc5296860df79fdc5a6919eb27f1f9a748d55
ohsung0722 <107835039+ohsung0722@users.noreply.github.com>
2024-03-05 16:15:44 +0900

    Title: 비교 연산, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240305/비교 연산/comparison-operator.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()));
+
+a = arr[0];
+b = arr[1];
+
+print("1" if a >= b else "0");
+print("1" if a > b else "0");
+print("1" if a <= b else "0");
+print("1" if a < b else "0");
+print("1" if a == b else "0");
+print("1" if a != b else "0");
